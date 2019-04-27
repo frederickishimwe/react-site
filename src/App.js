49a709cc28814eb14c28fc1e67154db65d556ddc
Ushimwe, Frederic: Absa <Frederic.Ushimwe@absa.africa>
2019-04-27 23:28:06 +0200

    updated text on app.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          v1.0 Learn React
         </a>
       </header>
     </div>
