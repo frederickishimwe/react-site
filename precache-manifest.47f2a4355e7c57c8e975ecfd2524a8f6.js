b961df861ffd0f5948bf1533574a6c5000fcb677
Ushimwe, Frederic: Absa <Frederic.Ushimwe@absa.africa>
2019-04-27 23:34:38 +0200

    Updates

---
+++ precache-manifest.47f2a4355e7c57c8e975ecfd2524a8f6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5dc26cdc462991bce6006f20d77edd3",
+    "url": "/react-site/index.html"
+  },
+  {
+    "revision": "16f51d4dee009c10ac58",
+    "url": "/react-site/static/css/main.584f321a.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/react-site/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "16f51d4dee009c10ac58",
+    "url": "/react-site/static/js/main.966776ed.chunk.js"
+  },
+  {
+    "revision": "d755518085d7bc56e74a",
+    "url": "/react-site/static/js/runtime~main.c998a049.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-site/static/media/logo.5d5d9eef.svg"
+  }
+]);
